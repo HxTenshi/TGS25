18f0e40700e75022476e2c87c3155414ccb3c57e
jackal0024 <icemaunntenn@gmail.com>
2016-06-01 14:16:24 +0900

    更新

--- Engine/ScriptComponent/Scripts/PointItem.h
+++ Engine/ScriptComponent/Scripts/PointItem.h
@@ -3,7 +3,7 @@
 #include "main.h"
 
 
-class PlayerManager :public IDllScriptComponent{
+class PointItem :public IDllScriptComponent{
 public:
 	void Initialize()override;
 	void Start()override;
@@ -13,14 +13,6 @@ public:
 	void OnCollideEnter(Actor* target)override;
 	void OnCollideExit(Actor* target)override;
 
-	void CreditDown();
-	void CreditUp();
-	void ItemGet();
-
 private:
 	//メンバ変数
-	SERIALIZE
-	int mCredit; //プレイヤーの残機
-	SERIALIZE
-	int mMaxPoint; //回収するアイテムの数
 };
